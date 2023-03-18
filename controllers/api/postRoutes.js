61b5e5668ac6640206f3d18f69ed1104affe1ad8
alex.kim <alkkim@gmail.com>
2023-03-18 15:47:07 -0700

    create delete route for blog posts

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -16,3 +16,38 @@ router.post('/', withAuth, async (req,res) => {
         res.status(500).json(err);
     }
 })
+
+// update a previous blog post entry '/api/post/:id'
+router.put('/:id', withAuth, async (req, res) => {
+    try {
+        const updatePost = await Post.update({
+            title: req.body.title,
+            content: req.body.content,
+        },
+        {
+            where: {
+                id: req.params.id,
+            }
+        });
+        res.status(200).json(updatePost);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+})
+
+// delete a blog post '/api/post/:id'
+router.delete('/:id', withAuth, async (req, res) => {
+    try {
+        const deletePost = await Post.destroy({
+            where: {
+                id: req.params.id,
+                user_id: req.session.user_id
+            },
+        });
+        res.status(200).json(deletePost);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+})
