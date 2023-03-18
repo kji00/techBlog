90ab8f6ae760711149421ce5d4cf0c87961ad6f7
alex.kim <alkkim@gmail.com>
2023-03-18 15:17:10 -0700

    created post route for new blog post entries

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -2,3 +2,17 @@ const router = require('express').Router();
 const { User, Post, Comment } = require('../../models');
 const withAuth = require('../../utils/auth');
 
+// create a new blog post entry '/api/post'
+router.post('/', withAuth, async (req,res) => {
+    try {
+        const newPost = await Post.create({
+            title: req.body.title,
+            content: req.body.content,
+            user_id: req.session.user_id,
+        });
+        res.status(200).json(newPost);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+})
