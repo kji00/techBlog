602c9dec92d63840dbe072285b621f3848255e27
alex.kim <alkkim@gmail.com>
2023-03-18 21:14:32 -0700

    created routes for homepage

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -51,3 +51,5 @@ router.delete('/:id', withAuth, async (req, res) => {
         res.status(500).json(err);
     }
 })
+
+module.exports = router;
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,10 +2,89 @@ const router = require('express').Router();
 const sequelize = require('../config/connection');
 const { Post, User, Comment } = require('../models');
 
-// GET route for all posts for homepage
-router.get('/', (req, res) => {
-    console.log('get route hit!')
-})
+// GET route for all posts for homepage '/'
+router.get('/', async (req, res) => {
+    try {
+        const postData = await Post.findAll({
+            attributes: [
+                'id',
+                'title',
+                'content',
+                'created_at'
+            ],
+            include: [
+                {
+                    model: Comment,
+                    attributes: ['id', 'comment', 'post_id', 'user_id', 'created_at'],
+                    include: {
+                        model: User,
+                        attributes: ['username'],
+                    },
+                },
+            ],
+            // set posts in descending order
+            order: [
+                ['created_at', 'DESC']
+            ],
+        });
+
+        const post = postData.map(post => post.get({ plain: true }));
+
+        res.render('homepage', {
+            post,
+            loggedIn: req.session.loggedIn,
+            username: req.session.username,
+            user_id: req.session.user_id
+        })
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
+
+// get user post by id '/post/:id'
+router.get('/post/:id', async (req, res) => {
+    try {
+        const postData = await Post.findOne({
+            where: {
+                id: req.params.id
+            },
+            attributes: [
+                'id',
+                'content',
+                'title',
+                'created_at'
+            ],
+            include: [
+                {
+                    model: Comment,
+                    attributes: [
+                        'id',
+                        'comment',
+                        'post_id',
+                        'user_id',
+                        'created_at'
+                    ],
+                    include: {
+                        model: User,
+                        attributes: ['username'],
+                    },
+                },
+            ],
+        });
+
+        const post = postData.map(post => post.get({ plain: true }));
+
+        res.render('single-post', {
+            post,
+            loggedIn: req.session.loggedIn,
+            username: req.session.username,
+        })
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }   
+});
 
 
 // Login page for user
